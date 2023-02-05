a12df83d3cb562fde01d67e9f989b8bb631c29ac
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2023-02-05 22:45:04 +0300

    Add label

--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Выберите категорию помощи"
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        setConstraints()
     }
-
-
+    
+    private func setConstraints() {
+        view.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 17),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
 }
 
